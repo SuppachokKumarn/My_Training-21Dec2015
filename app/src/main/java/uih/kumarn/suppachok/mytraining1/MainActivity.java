2817cbee9e175b388c8ad8fb440ec37b00fa26c6
Flukekue <flukekue@hotmail.com>
2015-12-21 16:41:32 +0700

    commit day1

--- app/src/main/java/uih/kumarn/suppachok/mytraining1/MainActivity.java
+++ app/src/main/java/uih/kumarn/suppachok/mytraining1/MainActivity.java
@@ -1,13 +1,30 @@
 package uih.kumarn.suppachok.mytraining1;
 
+import android.provider.LiveFolders;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    //Explicit ประกาศตัวแปล
+    private ListView trafficListView;
+    private Button aboutMeButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        bindWidget();
+
+    }   //Main Method
+
+    private void bindWidget() {
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Botton) findViewById(R.id.button)
     }
-}
+
+}   //Main Class
